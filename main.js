3d19c39827226cbe58ffad968c416a275768a96a
Hallef Brendo Coelho <hallefcoelho@gmail.com>
2022-03-03 21:33:08 -0300

    update

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ const funcStorage = {
   }
 };
 
-const addButton = document.querySelector("#btn-to-add");
 
 let localStorageList = funcStorage.get()
 
 localStorageList.forEach(element => {
-  elementHTML(element)
+  elementHTML(element[0])
 });
+
 
 function elementHTML(toAddValue) {
   setTimeout(() => {
@@ -32,26 +32,53 @@ function elementHTML(toAddValue) {
     btn.addEventListener("click", () => {
       btn.parentNode.parentNode.removeChild(newList);
       const nodeText = btn.parentNode.childNodes[2];
-
       let comparisonList = [];
       comparisonList.push(nodeText.data);
-      const index = localStorageList.indexOf(comparisonList[0]);
+      const found = localStorageList.find(element => element[0] == comparisonList);
+
+      const index = localStorageList.indexOf(found);
 
       localStorageList.forEach(element =>{
-        if(element === comparisonList[0]){
-          localStorageList.splice(index, 1);
+        if(element === found){
+          localStorageList.splice(index, 1)
         };
       });
       funcStorage.set(localStorageList);
+
     });
 
     const checkbox = document.createElement("input");
     checkbox.classList.add("checkbox");
     checkbox.setAttribute("type", "checkbox");
     newList.appendChild(checkbox);
-    checkbox.addEventListener("input", () => {
-      checkbox.checked ? newList.classList.add("list-made") : newList.classList.remove("list-made");
+    checkbox.addEventListener("change", () => {
+      const found = localStorageList.find(element => element[0] == toAddValue);
+      const index = localStorageList.indexOf(found);
+      
+      localStorageList.forEach(element => {
+        if (element === found) {
+          if(checkbox.checked){
+            newList.classList.add("list-made")
+            localStorageList[index].splice(1, 1, true)
+          }
+          else {
+            newList.classList.remove("list-made");
+            localStorageList[index].splice(1, 1, false)
+          }
+          funcStorage.set(localStorageList);
+        }
+      })
     })
+    const found = localStorageList.find(element => element[0] == toAddValue);
+
+    if (found[1] === true) {
+      newList.classList.add("list-made")
+      checkbox.checked = true
+    }
+    else {
+      newList.classList.remove("list-made");
+      checkbox.checked = false
+    }
 
     const text = document.createTextNode(toAddValue);
     newList.appendChild(text);
@@ -64,13 +91,12 @@ function elementHTML(toAddValue) {
 }
 
 function addList() {
-
   const toAdd = document.querySelector("#to-add");
     if(!toAdd.value == ""){
       
       elementHTML(toAdd.value);
   
-      localStorageList.push(toAdd.value);
+      localStorageList.push([toAdd.value, false]);
   
       funcStorage.set(localStorageList);
     }
@@ -78,5 +104,8 @@ function addList() {
   toAdd.value = "";
 
 }
+
+
+const addButton = document.querySelector("#btn-to-add");
 
 addButton.addEventListener("click", addList)
